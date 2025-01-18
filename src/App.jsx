230a17b32cfaaa1b2371b03cbe2c7570e1a30d24
AssiaChemlali <chemlali.assia@gmail.com>
2025-01-18 13:33:16 +0100

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './App.css'
 import Navbar from './components/Navbar'
 import {Route,Routes } from 'react-router'
 import Home from './pages/Home'
+import Trending from './pages/Trending'
+import MostPopular from './pages/MostPopular'
+import About from './pages/About'
+import Login from './pages/Login'
+
 function App() {
-
-
   return (
     <>
       <div className='px-4 md:px-8 xl:px-32 2xl:px-64'>
@@ -13,6 +16,10 @@ function App() {
       </div>
       <Routes>
           <Route path ="/" element={<Home/>}/>
+          <Route path ="/trending" element={<Trending/>}/>
+          <Route path ="/mostPopular" element={<MostPopular/>}/>
+          <Route path ="/about" element={<About/>}/>
+          <Route path ="/login" element={<Login/>}/>
       </Routes>
 
      </>
