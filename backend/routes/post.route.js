32f9ba62f97f8a07e419d866d15bf15d5815ac92
AssiaChemlali <chemlali.assia@gmail.com>
2025-01-24 11:52:34 +0100

    create post

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getPosts,getPost } from "../controlles/post.controller.js";
+import { getPosts,getPost,createPost } from "../controlles/post.controller.js";
 const router=express.Router()
 
 router.get("/",getPosts)
 router.get("/:slug",getPost)
+router.post("/",createPost)
 export default router
