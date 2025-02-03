a385ee6e5764c8b39f97bdcc86ed99a8f576a57f
AssiaChemlali <chemlali.assia@gmail.com>
2025-02-03 11:52:24 +0100

    fix image and video upload

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,15 +1,30 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import {CATEGORIES}from '../lib/index'
 import { useAuth, useUser } from '@clerk/clerk-react'
 import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill'
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router';
+import { toast } from 'react-toast'
+import { IKContext, IKImage, IKUpload } from 'imagekitio-react';
+import Upload from '../components/Upload';
 
-import { ToastContainer, toast } from 'react-toast'
+
 const Write = () => {
   const {isLoaded,isSignedId}=useUser()
   const [value, setValue] = useState('');
+  const [cover, setCover] = useState('');
+  const [image, setImage] = useState('');
+  const [video, setVideo] = useState('');
+  const [progress, setProgress] = useState(0);
+
+   useEffect(()=>{
+    image && setValue(prev=>prev+`<p><image src="${image.url}"/></p>`)
+   },[image])
+
+   useEffect(()=>{
+   video && setValue(prev=>prev+`<p><iframe class="ql-video" src="${video.url}"/></p>`)
+   },[video])
 
   const navigate=useNavigate()
   const {getToken}=useAuth()
@@ -40,6 +55,7 @@ const Write = () => {
     e.preventDefault()
     const formData=new FormData(e.target)
     const data={
+      img:cover.path || "",
       title:formData.get('title'),
       category:formData.get('category'),
       desc:formData.get('desc'),
@@ -48,13 +64,18 @@ const Write = () => {
     mutation.mutate(data)
     console.log(data)
   }
+
+  
+
   return (
     <div className='w-full h-[calc(100vh - 64px)] md:h-[calc(100vh - 80px)] flex flex-col gap-6 '>
        <p className='text-xl font-light'>Create a New Post</p>
        <form onSubmit={handleSubmit} className="flex flex-col gap-6 h-full"> 
-         
-        <button className='bg-gray-50 shadow-md  text-gray-600 rounded-xl py-2 px-3 capitalize text-base w-max '>add a cover image</button>
 
+       
+        <Upload type="image" setProgress={setProgress} setData={setCover}>
+         <button className='bg-gray-50 shadow-md  text-gray-600 rounded-xl py-2 px-3 capitalize text-base w-max '>add a cover image</button>
+        </Upload>
         <input 
         className='text-gray-700 text-4xl py-2 font-semibold capitalize bg-transparent outline-none'
         type="text" 
@@ -78,16 +99,18 @@ const Write = () => {
 
         <textarea name="desc" id="" placeholder='A Short description' className='bg-white p-4  rounded-xl shadow-md'></textarea>
 
-        <div className='flex'>
+        <div className='flex flex-1'>
           <div className="flex flex-col gap-2 mr-2">
-            <div className="cursor-pointer">🖼️</div>
-            <div className="cursor-pointer">📹</div>
+            <Upload type="image" setProgress={setProgress} setData={setImage}>🖼️</Upload>
+            <Upload type="video" setProgress={setProgress} setData={setVideo}>📹</Upload>
           </div>
+
           <ReactQuill 
           theme="snow"  
-          className='flex-1 rounded-xl bg-white shadow-md h-full' 
+          className='flex-1 rounded-xl bg-white shadow-md' 
           value={value} 
           onChange={setValue}
+          readOnly={0 < progress && progress < 100}
           />
         </div>
      
@@ -95,11 +118,12 @@ const Write = () => {
        
         
         <button 
-          disabled={mutation.isPending}
+          disabled={mutation.isPending || (0<progress && progress<100)}
           className='bg-blue-800 text-white py-2 px-4 rounded-xl font-medium w-max disabled:bg-blue-400 disabled:cursor-not-allowed'>
           {mutation.isPending ? 'Loading...': 'send'}
           
         </button>
+        {"Progress:" + progress}
         {mutation.isError &&  <div>An error occurred: {mutation.error.message}</div>}
 
        </form>
